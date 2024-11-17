ee6b7072accf859b60aa7467130c9f1203f69d6b
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-11-17 19:02:54 +0000

    Add models/base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Base Model Module"""
+
+# from models import storage
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """A class representating BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes BaseModel class instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            # storage.new(self)
+
+    def __str__(self):
+        """String representation of BaseModel instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates public instance attribute ``updated_at`` with the
+        current datetime
+        """
+        self.updated_at = datetime.now()
+        # storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
