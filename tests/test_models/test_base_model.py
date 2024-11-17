4b1240c5aa5c13a9f7d1a9371e9154b5e1192bf3
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-11-17 21:26:23 +0000

    Update docstring

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,7 +9,7 @@ class TestBaseModel(unittest.TestCase):
     """Test Suite"""
 
     def setUp(self):
-        """Setup instances of BaseModel"""
+        """Setup instances of BaseModel class for tests"""
         self.obj_dict = {"id": "7ae56a93-0712-43db-bd33-14d63aa60184",
                          "created_at": "2024-11-12T16:13:19.651327",
                          "updated_at": "2024-11-12T16:13:19.651371"
